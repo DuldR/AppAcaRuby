b388ecf7f93f8a8ab18fbeeece71923a36f9d0bf
Garrett Julaka <Garrett_Julaka@hma.honda.com>
2019-10-04 14:21:43 -0500

    Recursion is VERY unintuitive.
    Had to look up solutions for both merge and binary search.
    Problem set didnt give very good descriptions on both algorithms. Binary and merge_sort revolve around dividing and conquering a sort problem.
    
    Binary Search will only work on a SORTED array.

--- Recursion/Exercises/problemset.rb
+++ Recursion/Exercises/problemset.rb
@@ -71,3 +71,89 @@ def deep_dup(arr)
     returnArr
 
 end
+
+#Fib Number
+
+def fib(n)
+
+    return 1 if n == 1 || n == 2
+
+    fib(n-1) + fib(n-2)
+
+end
+
+
+
+def binary_search(array, key, low=0, high=array.size-1)  
+    return nil if low > high  
+    mid = (low + high) / 2  
+    return mid if array[mid]==key  
+    if array[mid] > key  
+      high = mid - 1  
+    else  
+      low = mid + 1  
+    end 
+
+    binary_search(array, key, low, high)   
+end   
+
+# def bin(arr, n)
+#     value = []
+
+#     if arr[0] != n
+#         value << 
+#     else
+#         value << bin(arr[1..-1], n)
+#     end
+
+# end
+
+
+# def bin(arr, n)
+#     value = 0
+
+#     if arr[0] == n
+#         return 0
+#     elsif arr.length == 0
+#         return 1
+#     else
+#         value = (1 + bin(arr[1..-1], n))
+#     end
+
+#     return value
+# end
+
+def merge_sort(arr)
+
+    #If the arr is 1 or 0, you've reached your base case.
+    return arr if arr.length <= 1
+
+    mid = arr.length / 2
+
+    #Splitting the array in TWAIN
+    # ... is used because you dont want to include mid nor the actual arr length.
+    left_side = arr[0...mid]
+    right_side = arr[mid...arr.length]
+
+    #Merge calls the inductive step.
+    merge(merge_sort(left_side), merge_sort(right_side))
+
+end
+
+def merge(left, right)
+    sorted = []
+
+    #Sorts the arrays till you're empty.
+    until left.empty? || right.empty?
+        if left.first > right.first
+            sorted << right.shift
+        else
+            sorted << left.shift
+        end
+    end
+
+    #You must add the sorted and remaining arrays together.
+    #This ensures you've sorted it correctly.
+    sorted + left + right
+
+end
