8e1ae82a6fc47cdcefb41a38be53790170c133b2
DuldR <gnjulaka@gmail.com>
2019-10-10 21:50:09 -0500

    Completed WordChainer
    Works like solution. Different paths depending on words.
    Signed-off-by: DuldR <gnjulaka@gmail.com>

--- Recursion/Projects/WordChain/lib/wordchain.rb
+++ Recursion/Projects/WordChain/lib/wordchain.rb
@@ -9,6 +9,10 @@ class WordChainer
         @dictionary = Set.new
         @letters = ("a".."z").to_a
 
+        # Might need to comment this out based on comp
+        #Dir.chdir(File.dirname("/mnt/c/Users/VF034781/Documents/TestBed/Recursion/Projects/WordChain/lib/dictionary.txt"))
+        # ---------------------------------------- #
+        
         File.open("dictionary.txt").each do |line|
             @dictionary << line.chomp
         end
@@ -39,28 +43,44 @@ class WordChainer
     def run(source, target)
 
         @current_words = [source]
-        @all_seen_words = [source]
+        @all_seen_words = { source => nil }
 
         until @current_words.empty?
-            new_current_words = []
-
-            @current_words.each do |ele|
-
-                adjacent_words(ele).each do |wrd|
-
-                    if @all_seen_words.include?(wrd)
-                        next
-                    else
-                        @all_seen_words << wrd
-                        new_current_words << wrd
-                    end
-                end
-
-            end
-            @current_words = new_current_words
+            explore_current_words(@current_words)
         end
 
-        @all_seen_words
+        build_path(target)
+    end
+
+    def explore_current_words(arr)
+        new_current_words = []
+
+        arr.each do |ele|
+            adjacent_words(ele).each do |wrd|
+
+                if @all_seen_words.include?(wrd)
+                    next
+                else
+                    @all_seen_words[wrd] = ele
+                    new_current_words << wrd
+                end
+            end
+        end
+
+        @current_words = new_current_words
+    end
+
+    def build_path(target)
+        path = []
+
+        orig = target
+        
+        until @all_seen_words[orig] == nil
+            path << orig
+            orig = @all_seen_words[orig]
+        end
+
+        path << @all_seen_words.key(nil)
 
 
     end
