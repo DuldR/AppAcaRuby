3a88a737e39ca559633ebecaced9182224bbe9ff
DuldR <gnjulaka@gmail.com>
2019-10-31 12:23:57 -0500

    Stack completed

---
+++ DataStructures/Homework/DIYADTs/stack.rb
@@ -0,0 +1,22 @@
+class Stack
+    def initialize
+      # create ivar to store stack here!
+      @ivar = []
+
+    end
+
+    def push(el)
+      # adds an element to the stack
+      @ivar << el
+    end
+
+    def pop
+      # removes one element from the stack
+      @ivar = @ivar[0...-1]
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      @ivar[-1]
+    end
+  end
