d609eddbeabae3281b0b6b0eecf2f2674621f5a7
DuldR <gnjulaka@gmail.com>
2019-11-01 22:18:52 -0500

    Complete the initiailization and parent set method

--- DataStructures/Projects/PolyTreeNode/skeleton/skeleton/lib/00_tree_node.rb
+++ DataStructures/Projects/PolyTreeNode/skeleton/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+
+
+    end
+
+    def parent=(parent)
+
+        self.parent.children.delete(self) if @parent.nil? == false
+
+        @parent = parent
+
+
+
+        if parent.nil? == false && self.parent.children.include?(self) == false 
+            self.parent.children << self
+        end
+
+    end
+
+
 end
+
+node1 = PolyTreeNode.new("root")
+node2 = PolyTreeNode.new("child1")
+node3 = PolyTreeNode.new("child2")
