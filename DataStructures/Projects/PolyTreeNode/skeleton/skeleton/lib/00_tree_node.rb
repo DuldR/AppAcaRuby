c2aa83e3616f379e036741d583bf3099908d7775
DuldR <gnjulaka@gmail.com>
2019-11-04 14:56:25 -0600

    Completed HTML project and updated file structure for knights

--- DataStructures/Projects/PolyTreeNode/skeleton/skeleton/lib/00_tree_node.rb
+++ DataStructures/Projects/PolyTreeNode/skeleton/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,12 @@
 class PolyTreeNode
 
+    attr_reader :parent, :value, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+
+    end
+    
 end
