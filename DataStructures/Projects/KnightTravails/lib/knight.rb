fb181a13237ca91f80ff9df56c27693331f04d09
DuldR <gnjulaka@gmail.com>
2019-11-06 13:59:15 -0600

    Added new move position instance method

--- DataStructures/Projects/KnightTravails/lib/knight.rb
+++ DataStructures/Projects/KnightTravails/lib/knight.rb
@@ -1,31 +1,46 @@
 class KnightPathFinder
 
+    def self.valid_moves(pos)
+
+        x, y = pos
+        movesR = []
+    
+        rows = [-2, 2]
+        cols = [-1, 1]
+    
+        rows.each do |row|
+            cols.each do |col|
+                movesR << [x+row, y+col]
+                movesR << [x+col, y+row]
+            end
+        end
+    
+        return movesR.select.with_index do |arr, idx|
+            arr if arr.all? { |item| item < 8 && item > 0 }
+        end
+    
+    end
 
     def initialize(pos)
         @root = pos
+        @considered_positions = []
 
     end
 
-    def valid_moves(pos)
+    def new_move_positions(pos)
+        new_moves = []
 
-        x, y = pos
-        moves = []
-        movesR = []
-    
-        [-1,1].each do |item|
-            movesR << [x+2, y+item]
-            movesR << [x-2, y+item]
+        self.valid_moves(pos).each do |p|
+            if @considered_positions.include?(p)
+                next
+            else
+                new_moves << p
+                @considered_positions << p
+            end
         end
-    
-        moves = movesR.clone
-    
-        movesR.each do |i|
-            moves << i.reverse
-        end
-    
-        return moves
+
+        new_moves
     end
-
 
 
 end
