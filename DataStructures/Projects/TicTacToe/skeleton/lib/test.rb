37d2e7c6580d68cd714dd8d6ee92d7a7a2019218
DuldR <gnjulaka@gmail.com>
2019-11-10 22:46:13 -0600

    A class can call itself to make another class. Huh.

---
+++ DataStructures/Projects/TicTacToe/skeleton/lib/test.rb
@@ -0,0 +1,11 @@
+class Butts
+
+
+    def initialize
+        @anus
+    end
+
+    def huh
+        @anus = [1,2]
+    end
+end
--- DataStructures/Projects/TicTacToe/skeleton/lib/tic_tac_toe_node.rb
+++ DataStructures/Projects/TicTacToe/skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark
+  attr_reader :board, :next_mover_mark, :children
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
@@ -17,5 +17,19 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    pot_moves = []
+
+    if @board.rows.all? {|col| col.all?(&:nil?) }
+        # (0..2).each do |row|
+        #   (0..2).each do |col|
+        #     pot_moves << [row, col]
+        #   end
+        # end
+      9.times { pot_moves << TicTacToeNode.new(board, :o)}
+    end
+
+    
+    pot_moves
+
   end
 end
