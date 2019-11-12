b18396efc71021569658a8d44635286e687c4ad7
DuldR <gnjulaka@gmail.com>
2019-11-11 21:17:36 -0600

    Needed to set up prev_move_pos as an instance variable

--- DataStructures/Projects/TicTacToe/skeleton/lib/tic_tac_toe_node.rb
+++ DataStructures/Projects/TicTacToe/skeleton/lib/tic_tac_toe_node.rb
@@ -1,11 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark, :children
+  attr_reader :board, :next_mover_mark, :children, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -18,13 +19,18 @@ class TicTacToeNode
   # the current move.
   def children
     pot_moves = []
+    prev_pos = [0, 1, 2].product([0, 1, 2])
 
     if @board.rows.all? {|col| col.all?(&:nil?) }
 
       if @next_mover_mark == :o
-        9.times { pot_moves << TicTacToeNode.new(board, :x)}
+        prev_pos.each do |pos|
+          pot_moves << TicTacToeNode.new(board.dup, :x, pos)
+        end
       else
-        9.times { pot_moves << TicTacToeNode.new(board, :o)}
+        prev_pos.each do |pos|
+          pot_moves << TicTacToeNode.new(board.dup, :o, pos)
+        end
       end
 
     end
