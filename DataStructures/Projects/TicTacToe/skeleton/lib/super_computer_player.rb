4074b7cd4ec90c3f0a37799d8631b27d68c04a81
DuldR <gnjulaka@gmail.com>
2019-11-18 15:43:25 -0600

    This is all jacked up. Rspec is passing but game isn't working correctly.

--- DataStructures/Projects/TicTacToe/skeleton/lib/super_computer_player.rb
+++ DataStructures/Projects/TicTacToe/skeleton/lib/super_computer_player.rb
@@ -2,12 +2,50 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
-    test_node = TicTacToeNode.new(game, :x)
-    
+
+    node = TicTacToeNode.new(game.board, mark)
+
+    # The computer looks gets the array of places it can move to. We
+    # shuffle the array so that the computer will play differently
+    # each time (though it will always make a winning move if
+    # possible, and a non-losing move if there are no winning moves).
+    #
+    # The shuffling is optional.
+    possible_moves = node.children.shuffle
+
+    # If any move results in a #winning_node? we want to choose that
+    # one. Find picks the first of the winning moves in
+    # `possible_moves`.
+    node = possible_moves.find { |child| child.winning_node?(mark) }
+
+    # Stop if we found a winner. `node` may be `nil` if there are no
+    # winning moves to make.
+    return node.prev_move_pos if node
+
+    # Maybe there is no winning move. Then at least don't pick a
+    # loser.
+    node = possible_moves.find { |child| !child.losing_node?(mark) }
+
+    return node.prev_move_pos if node
+
+    # If the computer plays perfectly, we should never be able to
+    # force it to lose. Let's raise an alarm in that case!
+    raise "Wait, it looks like I'm going to lose?"
+  
+
+
+    # test_node = TicTacToeNode.new(game.board, mark)
+
+    # pot_moves = test_node.children.select { |node| node.board.winner == mark }
+
+    # pick_move = pot_moves.find { |child| child.winning_node?(mark) }
+    # return pick_move.prev_move_pos if pick_move
+
+    # pick_move = pot_moves.find { |child| !child.losing_node?(mark) }
+    # return node.prev_move_pos if pick_move
+
   end
 
-  def win_move?(node)
-  end
 end
 
 if __FILE__ == $PROGRAM_NAME
