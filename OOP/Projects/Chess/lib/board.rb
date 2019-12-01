6b813b7373425910c29b47c48f3d0a3d75fdd9eb
DuldR <gnjulaka@gmail.com>
2019-11-30 22:39:01 -0600

    Added grid manipulation.

--- OOP/Projects/Chess/lib/board.rb
+++ OOP/Projects/Chess/lib/board.rb
@@ -4,15 +4,36 @@ class Board
 
     def initialize
         @rows = Array.new(8) { Array.new(8) }
-        @rows = @rows.map.with_index do |p, idx|
-            p.map do |pos|
-                if idx == 0 || idx == 1
+        @rows = @rows.map.with_index do |row, idx|
+            row.map do |pos|
+                if idx == 0 || idx == 1 || idx == 6 || idx == 7
                     pos = Piece.new
                 else
                     pos
                 end
             end
         end
+    end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+    def valid_pos?(pos)
+        x, y = pos
+
+        if x < 0 || x > 8 || y < 0 || y > 8
+            return false
+        end
+
+        return true
+
     end
 
 end
@@ -30,3 +51,5 @@ class Piece
     end
 
 end
+
+b = Board.new
