078ee66d1fb99c045810b233586d3087fd32bdb1
DuldR <gnjulaka@gmail.com>
2019-12-01 22:27:27 -0600

    Added slideable module. It can use the subclass instance variables?

--- OOP/Projects/Chess/lib/pieces/slideable.rb
+++ OOP/Projects/Chess/lib/pieces/slideable.rb
@@ -1,16 +1,59 @@
 module Slideable
+    HORIZONTAL_AND_VERTICAL_DIRS = [
+        [-1, 0],
+        [0, -1],
+        [0, 1],
+        [1, 0]
+    ].freeze
+        
+    DIAGONAL_DIRS = [
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+        [1, 1]
+    ].freeze
+
+    def hor_and_ver
+        HORIZONTAL_AND_VERTICAL_DIRS
+    end
+
+    def diag
+        DIAGONAL_DIRS
+    end
 
     def moves
+        moves = []
 
+        move_dirs.each do |dx, dy|
+            moves.concat(find_valid_moves(dx,dy))
+        end
 
-
+        moves
     end
 
     def move_dirs
-        return :hor
+        raise NotImplementedError
     end
 
+    def find_valid_moves(dx, dy)
+        cur_x, cur_y = pos
+        moves = []
 
+        loop do
+            cur_x, cur_y = cur_x + dx, cur_y + dy
+            pos = [cur_x, cur_y]
+
+            break unless board.valid_pos?(pos)
+
+            if board.empty?(pos)
+                moves << pos
+            else
+                moves << pos if board[pos].color != color
+                break
+            end
+        end
+
+        moves
+    end
 end
-
 
