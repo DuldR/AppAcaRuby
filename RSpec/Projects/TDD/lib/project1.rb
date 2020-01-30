fe817271267a624ca47ec4ef5947acd6952991f3
DuldR <gnjulaka@gmail.com>
2020-01-29 20:30:55 -0600

    Completed my_uniq. MUST study rspec syntax it's can cause false positives.

--- RSpec/Projects/TDD/lib/project1.rb
+++ RSpec/Projects/TDD/lib/project1.rb
@@ -1,3 +1,17 @@
+#  [1, 2, 1, 3, 3].uniq # => [1, 2, 3]
+
 def my_uniq(arr)
     raise ArgumentError unless arr.is_a?(Array)
+    returnArr = []
+
+    arr.each do |x|
+        if returnArr.include?(x)
+            next
+        else
+            returnArr << x
+        end
+    end
+
+    returnArr
+
 end
--- RSpec/Projects/TDD/spec/project1_spec.rb
+++ RSpec/Projects/TDD/spec/project1_spec.rb
@@ -3,7 +3,23 @@ require "project1"
 describe "Part 1:" do
     describe "my_uniq" do
         it "raise an error if not an array" do
-        expect { my_uniq("Hello") }.to raise_error(ArgumentError)
+            expect { my_uniq("Hello") }.to raise_error(ArgumentError)
+        end
+
+        it "must not return duplicates" do
+            expect(my_uniq([1, 1])).to eq([1])
+        end
+
+        it "must return in order found" do
+            expect(my_uniq([2, 1, 3, 2])).to eq([2, 1,3])
         end
     end
 end
+
+#### EXAMPLES
+
+# it "should return the average of the two numbers" do
+#     expect(average(4, 8)).to eq(6)
+#     expect(average(5, 10)).to eq(7.5)
+
+###########
