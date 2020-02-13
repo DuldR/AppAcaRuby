32120f14d565ee54534d8c3326b246bf50770b83
DuldR <gnjulaka@gmail.com>
2020-02-12 21:42:42 -0600

    Context was the issue.

--- RSpec/Projects/Poker/lib/card.rb
+++ RSpec/Projects/Poker/lib/card.rb
@@ -1,3 +1,26 @@
 class Card
 
+    attr_reader :face, :suit
+
+    def initialize(face, suit)
+        numbers = [2,3,4,5,6,7,8,9,10]
+        face_cards = ["Jack", "Queen", "King", "Ace"]
+
+        
+        # if numbers.include?(face.to_i) || face_cards.include?(face)
+        #     @face = face
+        # else
+        #     raise "That's not a face"
+        # end
+
+        if numbers.include?(face.to_i) || face_cards.include?(face)
+            @face = face
+        else
+            raise "That's not a face"
+        end
+
+    end
+
+
+
 end
--- RSpec/Projects/Poker/spec/card_spec.rb
+++ RSpec/Projects/Poker/spec/card_spec.rb
@@ -1,11 +1,24 @@
 require 'card'
 
 describe Card do
-    subject(:card) { Card.new }
+    subject(:card) { Card.new("4", "Spade") }
 
     describe '#initialize' do
         it 'must set a suit' do
-            
+            expect(card.suit).to eq("Spade")
         end
+
+        it 'must set a face' do
+            expect(card.face).to eq("4")
+        end
+
+        it "when suit or face is not valid" do
+            expect{ Card.new("4", "what") }.to raise_error("That's not a suit")
+            expect{ Card.new("hello", "Spade") }.to raise_error("That's not a face")
+        end
+
+
     end
+
+
 end
