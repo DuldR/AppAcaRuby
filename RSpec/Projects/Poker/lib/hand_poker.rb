9368c05e1561e6facf3a6865b4fd84f06959c5fe
DuldR <gnjulaka@gmail.com>
2020-02-20 15:48:39 -0600

    Need to use uniq to eval pairs. Need to sort the nil vals out of the uniq array

--- RSpec/Projects/Poker/lib/hand_poker.rb
+++ RSpec/Projects/Poker/lib/hand_poker.rb
@@ -11,7 +11,49 @@ class Hand_Poker
     end
 
     def hand_rank
-        
+
+
+    end
+
+    def hand_pairs
+        pairs = []
+        score = []
+
+        @hand.each do |card|
+            if card.face.to_i == 0
+                next
+            else
+                pairs << card.face.to_i
+            end
+
+            pairs.sort
+        end
+
+
+
+        # if pairs.uniq.length == 2
+        #     pairs.uniq.each do |i|
+        #         score << pairs.count(i)
+        #     end
+
+        #     if score.include?(3) && score.include?(2)
+        #         return "Full house!"
+        #     end
+
+
+        # pairs.each do |pair|
+
+        #     if pairs.count(pair) == 4
+        #         return 4
+        #     elsif pairs.count(pair) == 3
+        #         return 3
+        #     elsif pairs.count(pair) == 2
+        #         return 2
+        #     else
+        #         next
+        #     end
+        # end 
+
     end
 
 end
