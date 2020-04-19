c947beedbc1752fa746d91aa8b341adf5dbad228
DuldR <gnjulaka@gmail.com>
2020-04-19 00:20:28 -0500

    Has Set and Get are O(1) time complexity. This ensures using 2 loops without the needed nesting.

--- BigO/Projects/TwoSum/twosum.rb
+++ BigO/Projects/TwoSum/twosum.rb
@@ -2,8 +2,10 @@ arr = [0, 1, 5, 7]
 # two_sum?(arr, 6) # => should be true
 # two_sum?(arr, 10) # => should be false
 
+brr = [0, 4, 6, 3, 1, 100, 30, 40, 20, 5]
 
 
+#This is O(n^2). Nested loop.
 def bad_two_sum?(arr, target)
 
     arr.each.with_index do |x, idx|
@@ -22,5 +24,29 @@ def bad_two_sum?(arr, target)
 
 end
 
+
+def okay_two_sum?(arr, target)
+    arr.sort!
+    lhs = 0
+    rhs = arr.length - 1 
+
+    while lhs < rhs
+        total = arr[lhs] + arr[rhs]
+        if total > target
+            rhs -= 1
+        elsif total < target
+            lhs += 1
+        elsif total == target
+            return true
+        end
+
+    end
+
+    return false
+end
+
 p bad_two_sum?(arr, 6)
 p bad_two_sum?(arr, 10)
+print "\n"
+p okay_two_sum?(brr, 130)
+p okay_two_sum?(brr, 25)
