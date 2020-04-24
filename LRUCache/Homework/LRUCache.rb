08c7e972ea63ee45675bf3fd5e52d1a72930a56e
DuldR <gnjulaka@gmail.com>
2020-04-24 00:48:56 -0500

    Not too hard. Time complexity is O(n) due to the index function.

--- LRUCache/Homework/LRUCache.rb
+++ LRUCache/Homework/LRUCache.rb
@@ -1,20 +1,35 @@
   class LRUCache
-    def initialize
+    def initialize(size)
+        @size = size
+        @cache = []
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.size
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      idx = @cache.find_index(el) 
+
+      if idx == nil
+        @cache.unshift(el)
+        if self.count > @size
+            @cache.pop
+        end
+      else
+        @cache.delete_at(idx)
+        @cache.unshift(el)
+      end
+
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @cache
     end
 
     private
     # helper methods go here!
-
   end
