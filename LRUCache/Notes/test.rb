ca360bda4aaa7e614b31d873abf9612a49f3f81e
DuldR <gnjulaka@gmail.com>
2020-04-25 21:07:29 -0500

    Hashing is fun. Easiest way is to convert everything to an array and has that.

---
+++ LRUCache/Notes/test.rb
@@ -0,0 +1,22 @@
+class Butt
+
+    def initialize
+        @weiner = true
+    end
+
+    def hungry?
+        p @weiner
+
+        idk
+    end
+
+
+    private
+
+    def idk
+        p @weiner
+    end
+
+
+
+end
--- LRUCache/Projects/MyHashMap/lib/p02_hashing.rb
+++ LRUCache/Projects/MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    fixnum = self.join.to_i
+    fixnum ^ 2
   end
 end
 
 class String
   def hash
+    fixnum = self.chars.map { |x| x.ord }
+    fixnum.join.to_i ^ 2
   end
 end
 
@@ -16,6 +20,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    fixnum = []
+    self.each do |k, v|
+      fixnum << k.to_s
+      fixnum << v
+    end
+
+    fixnum = fixnum.map { |x| x.ord }
+    fixnum.sort!
+    fixnum.join.to_i ^ 2
   end
 end
