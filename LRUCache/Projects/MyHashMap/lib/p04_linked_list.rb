167529a3365e765fe732af019eae399af3d448d1
DuldR <gnjulaka@gmail.com>
2020-04-27 21:07:55 -0500

    Linked list completed. VERY interesting data structure. Get, include, remove are all O(n) or worse though.

--- LRUCache/Projects/MyHashMap/lib/p04_linked_list.rb
+++ LRUCache/Projects/MyHashMap/lib/p04_linked_list.rb
@@ -16,11 +16,21 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+
+    
   end
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +39,98 @@ class LinkedList
   end
 
   def first
+    if @head.next == @tail
+      return @head
+    else
+      return @head.next
+    end
   end
 
   def last
+    if @tail.prev == @head
+      return @tail
+    else
+      return @tail.prev
+    end
   end
 
   def empty?
+    if @head.next == @tail
+      return true
+    else
+      return false
+    end
   end
 
   def get(key)
+    return if self.include?(key) == false
+
+    self.each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+
   end
 
   def include?(key)
+
+    self.each do |node|
+      if node.key == key
+        return true
+      end
+    end
+    
+    return false
   end
 
   def append(key, val)
+    old_prev = @tail.prev
+    @tail.prev = Node.new(key, val)
+
+    @tail.prev.prev = old_prev
+    @tail.prev.next = @tail
+    @tail.prev.prev.next = @tail.prev
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+      end
+    end
   end
 
   def remove(key)
+
+    if self.include?(key) == true
+      self.each do |node|
+        if node.key == key
+          old_prev = node.prev
+          old_next = node.next
+
+          old_prev.next = old_next
+          old_next.prev = old_prev
+        end
+      end
+    end
+
+
   end
 
   def each
+    currentNode = self.first
+
+    until currentNode == @tail
+      yield currentNode
+      currentNode = currentNode.next
+    end
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
+
 end
