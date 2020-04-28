1d7d19833248bd59197c86c32a9c5ac949e06da1
DuldR <gnjulaka@gmail.com>
2020-04-28 14:43:09 -0500

    Part 1 complete. Yield(x,y) will present two variable that can be enacted upon by a block.

--- LRUCache/Projects/MyHashMap/lib/p05_hash_map.rb
+++ LRUCache/Projects/MyHashMap/lib/p05_hash_map.rb
@@ -1,7 +1,10 @@
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_accessor :count, :store
+
+  include Enumerable
+
+  attr_accessor :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -22,41 +25,69 @@ class HashMap
 
   def set(key, val)
 
+    if num_buckets == @count
+      resize!
+    end
+
     bucket(key).each do |node|
       if node.key == key
         node.val = val
+        return
       end
     end
 
     bucket(key).append(key,val)
+    @count += 1
       
   end
 
   def get(key)
 
-
     bucket(key).each do |node|
       return node.val if node.key == key
     end
-
 
     nil
 
   end
 
   def delete(key)
+
+    if include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    else
+      nil
+    end
+
   end
 
   def each
+
+    iter = 0
+
+    until iter == num_buckets
+      @store[iter].each do |node|
+        if node.key == nil
+          next
+        else
+          yield(node.key, node.val)
+        end
+      end
+      iter += 1
+    end
+
+ 
+
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -68,6 +99,7 @@ class HashMap
   end
 
   def resize!
+    (num_buckets).times { @store << LinkedList.new }
   end
 
   def bucket(key)
